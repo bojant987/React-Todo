d3dae049b45217eafecc3011abba25f402410596
Bojan Todorovic <bojant987@hotmail.com>
2017-02-10 12:24:21 +0100

    added static list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,34 @@
 var React = require("react");
+var TodoList = require("TodoList");
 
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return{
+      todos: [{
+        id: 1,
+        text: "Walk the dog"
+      },
+    {
+      id: 2,
+      text: "Feed the cat"
+    },
+    {
+      id: 3,
+      text: "Do this"
+    },
+    {
+      id: 4,
+      text: "Do that"
+    }
+  ]
+   };
+  },
   render: function() {
+    var {todos} = this.state;
     return(
-      <div>todo app</div>
+      <div>
+        <TodoList todos={todos}/>
+      </div>
     );
   }
 });
