d2656caeacfdaf7256d14940f01b433118aa9daf
Bojan Todorovic <bojant987@hotmail.com>
2017-02-11 16:45:16 +0100

    added todo filtering

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -46,9 +46,42 @@ describe("TodoAPI", () => {
       }];
       localStorage.setItem("todos", JSON.stringify(todos));
       var actualTodos = TodoAPI.getTodos();
-      
+
       expect(actualTodos).toEqual(todos);
     });
+  });
+
+  describe("filterTodos", () => {
+    var todos = [
+      {
+        id: 1,
+        text: "some text",
+        completed: true
+      },
+      {
+        id: 2,
+        text: "some other text",
+        completed: false
+      },
+      {
+        id: 3,
+        text: "some text",
+        completed: true
+      }
+    ];
+
+    it("should return all items if showCompleted is true", () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, "");
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it("should return non completed items if showCompleted is false", () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, "");
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
   });
 
 
