b2a5963c95a00f4c22d188a83f4768f3ca2c5b82
Bojan Todorovic <bojant987@hotmail.com>
2017-02-11 12:21:27 +0100

    added todo and todoapp tests

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -7,7 +7,23 @@ var TestUtils = require("react-addons-test-utils");
 var Todo = require("Todo");
 
 describe("Todo", () => {
+
   it("should exist", () => {
     expect(Todo).toExist();
   });
+
+  it("should call onToggle prop with id on click", () => {
+    var todoData = {
+      id: 101,
+      text: "blablabla",
+      completed: true
+    };
+    var spy = expect.createSpy();
+    var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(todo));
+
+    TestUtils.Simulate.click($el[0]);
+    expect(spy).toHaveBeenCalledWith(todoData.id);
+  });
+
 });
